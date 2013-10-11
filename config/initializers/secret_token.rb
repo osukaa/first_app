d91e2a0f16a4e426dbf295fbca79d7f6077b1cbb
Oscar Funes <osukaa18@gmail.com>
2013-10-11 14:28:10 -0600

    Restore secret token config file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '9aa28b6ea0533921a08771f0e8a9ceffd86a3e573ff6283e4a665a27eee6737e7431a05a2b3f683cf12ac5e70349b6eb20946e26cd01fab7810cea829a70c3e2'
